ee20120e59480a850ecdc58927dca3136b8b13a5
Kaid22 <avdickaid@gmail.com>
2021-04-13 15:07:29 +0200

    komita

--- kurs/Form1.cs
+++ kurs/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace kurs
 {
     public partial class Form1 : Form
@@ -15,6 +15,50 @@ namespace kurs
         public Form1()
         {
             InitializeComponent();
+        }
+        List<Kurs> lista = new List<Kurs>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Kurs temp = new Kurs();
+            int gotovo=1;
+            while (gotovo!=0)
+            {
+                gotovo = 0;
+
+                for (int i = 0; i < lista.Count - 1; i++)
+                {
+                    if (lista[i].SkupljiOd(lista[i + 1]))
+                    {                       
+                        temp = lista[i];
+                        lista[i] = lista[i + 1];
+                        lista[i + 1] = temp;
+                        gotovo += 1;
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                listBox1.Items.Add(lista[i]);
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader f = new StreamReader("kursevi.txt");
+            while (!f.EndOfStream)
+            {               
+                string naziv = f.ReadLine();
+                int cena = int.Parse(f.ReadLine());
+                Kurs k = new Kurs(naziv,cena);
+                listBox1.Items.Add(k);
+                lista.Add(k);
+            }
+            
         }
     }
 }
---
+++ kurs/kurs.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace kurs
+{
+    class Kurs
+    {
+        string naziv;
+        int cena;
+        public Kurs()
+        {
+            naziv = string.Empty;
+            cena = 0;
+        }
+        public Kurs(string naziv, int cena)
+        {
+            if (cena < 0)
+
+                throw new Exception("Cena mora da bude >=0");
+
+            if (naziv.Equals(string.Empty))
+
+                throw new Exception("Mora da ima naziv");
+
+            this.naziv = naziv;
+            this.cena = cena;
+        }
+        public void CitajIzFajla(StreamReader f)
+        {
+            naziv = f.ReadLine();
+            cena = Convert.ToInt32(f.ReadLine());
+            if (cena < 0 || naziv.Equals(string.Empty))
+                throw new Exception("Mora da ima naziv i cenu >=0");
+        }
+        public override string ToString()
+        {
+            return naziv + " - " + cena + "din";
+        }
+        public bool SkupljiOd(Kurs k)
+        {
+            return cena > k.cena;
+        }
+    }
+}
